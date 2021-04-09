b3a86ee7936cc4b9dc33df43419dcc00b97692a3
Zhanay Ilyassov <Zhanay86@gmail.com>
2021-04-09 12:40:49 +0600

    v1

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -3,9 +3,10 @@ package ru.netology.domain;
 public class Post {
     // data classes
     private String id;  // идентификатор поста
+    private String userId; // иденфикатор, владельца аккаунта
     private String userName; // имя, кто написал пост
     private String imageUrl; // Url картинки в посту
-    private String rePostid; // идентификатор репоста
+    private String reposterName; // идентификатор репоста
     private String rePostUserName; // имя, кто написал репост
 
 }
