8b16af038b83d7e4e240dc96aaa1fdd97927b9b3
Zhanay Ilyassov <Zhanay86@gmail.com>
2021-04-10 10:46:23 +0600

    v2'

---
+++ src/main/java/ru/netology/domain/CommentsInfo.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+public class CommentsInfo {
+    private int count;
+    private int canPost;
+    private int groupsCanPost;
+    private boolean can_close;
+    private boolean can_open;
+    private String likesInfo;
+    private String repostsInfo;
+    private String viewsInfo;
+
+
+}
--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -2,11 +2,25 @@ package ru.netology.domain;
 
 public class Post {
     // data classes
-    private String id;  // идентификатор поста
-    private String userId; // иденфикатор, владельца аккаунта
-    private String userName; // имя, кто написал пост
-    private String imageUrl; // Url картинки в посту
-    private String reposterName; // идентификатор репоста
-    private String rePostUserName; // имя, кто написал репост
+    private String id;
+    private String ownerId;
+    private String fromId;
+    private String createdBy;
+    private String date;
+    private String text;
+    private String replyOwnerId;
+    private String replyPostId;
+    private String frienlyOnly;
+    private int singerId;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+    private int isPinned;
+    private int marketAsAds;
+    private String postSourse;
+    private String attachments;
+    private String geo;
+    private String donut;
+    private int postponedId;
 
 }
